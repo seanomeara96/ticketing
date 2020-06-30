a5cd3cb0a517776ffab206c04fadea06b39484e6
Sean <seanom96@gmail.com>
2020-06-30 16:27:19 +0100

    implements validation middlewares

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
 const router = express.Router();
 router.post(
   "/api/users/signin",
@@ -11,11 +11,9 @@ router.post(
       .notEmpty()
       .withMessage("You must supply a password."),
   ],
+  validateRequest,
   (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
+    res.send({ message: "signed in" });
   }
 );
 export { router as signinRouter };
