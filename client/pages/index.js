7594f56ea89f3c723d5aa67bc633f02335fc9303
Sean <seanom96@gmail.com>
2020-07-01 22:33:23 +0100

    attempt to communicate with alt kubernetes service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,14 @@
-const LandingPage = ({ color }) => {
-  console.log(`I am in the component ${color}`);
+import axios from "axios";
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log("I am on the server");
-  return { color: "red" };
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get(
+    "http://auth-srv:3000/api/users/currentuser"
+  );
+  return response.data;
 };
 
 export default LandingPage;
