48b047f2b58a4fa2010853285c4047738df28a15
Sean <seanom96@gmail.com>
2020-07-01 11:17:15 +0100

    implemented global signin function

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+import request from "supertest";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "bobloblaw";
@@ -24,3 +34,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
