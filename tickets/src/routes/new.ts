f01f6350f6edc531c247c021a197f9d98abbb316
Sean <seanom96@gmail.com>
2020-07-05 13:28:20 +0100

    rout handler generates ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@somtickets/common";
+import { Ticket } from "../models/ticket";
 const router = express.Router();
 router.post(
   "/api/tickets",
@@ -12,8 +13,15 @@ router.post(
       .withMessage("Price must be greater than 0."),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+    res.status(201).send(ticket);
   }
 );
 export { router as createTicketRouter };
