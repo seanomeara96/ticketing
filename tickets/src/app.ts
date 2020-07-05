7437fe571c1b6aef82d1437de22c639bb9f7a1db
Sean <seanom96@gmail.com>
2020-07-05 20:11:23 +0100

    import and use indexTicketRouter

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import { errorHandler, NotFoundError, currentUser } from "@somtickets/common";
 import cookieSession from "cookie-session";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -17,6 +18,7 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 app.all("*", async () => {
   throw new NotFoundError();
 });
