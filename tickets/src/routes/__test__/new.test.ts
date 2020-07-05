ea3daeeab6b27a6fcd3576c08f863d65419b0e1b
Sean <seanom96@gmail.com>
2020-07-05 13:14:35 +0100

    should expect 0 tickets

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
-
+import { Ticket } from "../../models/ticket";
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
   expect(response.status).not.toEqual(404);
@@ -49,7 +49,8 @@ it("it returns an error if an invalid price is provided", async () => {
 
 it("creates a ticket with valid inputs", async () => {
   // Add in a check to make sure a ticket was saved
-
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
   await request(app)
     .post("/api/tickets")
     .send({
